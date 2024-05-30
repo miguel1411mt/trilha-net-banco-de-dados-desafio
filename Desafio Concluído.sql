3eeb3a6987b2c39c8100ebefefc73c7a10b12451
miguel1411mt <133440357+miguel1411mt@users.noreply.github.com>
2024-05-30 18:51:50 -0300

    Update and rename Desafio Concluido.sql to Desafio Concluído.sql

--- Desafio Concluído.sql
+++ Desafio Concluído.sql
@@ -53,7 +53,7 @@ INNER JOIN Generos G ON G.Id = FG.IdGenero
 SELECT F.Nome, G.Genero FROM Filmes F
 INNER JOIN FilmesGenero FG ON F.Id = FG.IdFilme
 INNER JOIN Generos G ON G.Id = FG.IdGenero
-WHERE G.Genero = 'Mistério'
+WHERE G.Genero = 'MistÃ©rio'
 
 -- 12
 
